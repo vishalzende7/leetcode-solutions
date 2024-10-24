d89af035b57eddfaf1f4515323de5b73b2f3e561
Vishal Zende <vishal.zende@neosoftmail.com>
2024-10-24 16:09:46 +0530

    IntToRoman Solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,11 @@ package org.example
 
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.pow
 
 fun main() {
-    println(maxArea(intArrayOf(1,8,6,2,5,4,8,3,7)))
+//    println(maxArea(intArrayOf(1,8,6,2,5,4,8,3,7)))
+    println(intToRoman(1994))
 }
 
 fun maxArea(height: IntArray): Int {
@@ -17,7 +19,7 @@ fun maxArea(height: IntArray): Int {
         val distance = right - left
         maxArea = max(smallestBar * distance, maxArea)
         if (height[left] < height[right]) left++
-         else if (height[right] <= height[left]) right--
+        else if (height[right] <= height[left]) right--
         else {
             left++
             right--
@@ -25,3 +27,33 @@ fun maxArea(height: IntArray): Int {
     }
     return maxArea
 }
+
+
+fun intToRoman(num: Int): String {
+    val romanMap = mutableMapOf<Int, String>(
+        1000 to "M",
+        900 to "CM",
+        500 to "D",
+        400 to "CD",
+        100 to "C",
+        90 to "XC",
+        50 to "L",
+        40 to "XL",
+        10 to "X",
+        9 to "IX",
+        5 to "V",
+        4 to "IV",
+        1 to "I",
+    )
+    val sb = StringBuilder()
+    var n = num
+    for ((v, s) in romanMap) {
+        val repeat = n / v
+        if(repeat >0){
+            sb.append(s.repeat(repeat))
+            n %= v
+        }
+    }
+    return sb.toString()
+}
+
