5d19fe7074419af99c42d1e68b19663bbdf876aa
Vishal Zende <vishal.zende@neosoftmail.com>
2024-10-24 12:34:49 +0530

    solved max-area-problem

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,27 @@
 package org.example
 
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
-    
+    println(maxArea(intArrayOf(1,8,6,2,5,4,8,3,7)))
+}
+
+fun maxArea(height: IntArray): Int {
+    var left = 0
+    var right = height.size - 1
+    var maxArea = 0
+
+    while (left < right) {
+        val smallestBar = min(height[left], height[right])
+        val distance = right - left
+        maxArea = max(smallestBar * distance, maxArea)
+        if (height[left] < height[right]) left++
+         else if (height[right] <= height[left]) right--
+        else {
+            left++
+            right--
+        }
+    }
+    return maxArea
 }
